3a6a3d88adc4a947fdb5e823c492782b0906bd74
Mr.Sky1001 <nikita.nk16@yandex.ru>
2020-03-14 20:29:51 +0600

    update

--- sfml-3d-game/sfml-3d-game/main.cpp
+++ sfml-3d-game/sfml-3d-game/main.cpp
@@ -35,7 +35,7 @@ int main()
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	cout << "Creating GLFW window..." << endl;
-	int widthW = 1336, heightW = 800;
+	int widthW = 1024, heightW = 800;
 	GLFWwindow* window = glfwCreateWindow(widthW, heightW, "sfml-3d-cube", nullptr, nullptr);
 	if (window == nullptr)
 	{
@@ -45,7 +45,6 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetKeyCallback(window, key_callback);
-
 
 	cout << "Initialize GLEW..." << endl;
 	glewExperimental = GL_TRUE;
@@ -58,26 +57,101 @@ int main()
 	cout << "Setting Viewport..." << endl;
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
-
 	glViewport(0, 0, width, height);
+	
+	cout << "Initialize shaders..." << endl;
+	GLfloat vertices[] = {
+	 0.5f,  0.5f, 0.0f,  // Верхний правый угол
+	 0.5f, -0.5f, 0.0f,  // Нижний правый угол
+	-0.5f, -0.5f, 0.0f,  // Нижний левый угол
+	-0.5f,  0.5f, 0.0f   // Верхний левый угол
+	};
+
+	GLuint indices[] = {  // Помните, что мы начинаем с 0!
+		0, 1, 3,   // Первый треугольник
+		1, 2, 3    // Второй треугольник
+	};
+
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	GLuint VAO;	
+	glGenVertexArrays(1, &VAO);
+	
+	// 1. Привязываем VAO
+	glBindVertexArray(VAO);
+	// 2. Копируем наши вершины в буфер для OpenGL
+	glBindBuffer(GL_ARRAY_BUFFER, VAO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. Копируем наши индексы в в буфер для OpenGL
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	// 3. Устанавливаем указатели на вершинные атрибуты
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// 4. Отвязываем VAO (НЕ EBO)
+	glBindVertexArray(0);
+
+
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	GLuint fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	if (!success)
+	{
+		glGetProgramInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::SHADERPROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	glUseProgram(shaderProgram);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
 
 	cout << "Start engine..." << endl;
-	// Игровой цикл
 	while (!glfwWindowShouldClose(window))
 	{
-		// Проверяем события и вызываем функции обратного вызова.
 		glfwPollEvents();
-
-		// Команды отрисовки здесь
-
-
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		// Меняем буферы местами
-		glfwSwapBuffers(window);
+
+		// 5. Отрисовываем объект
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		someOpenGLFunctionThatDrawsOurTriangle();
+		glBindVertexArray(0);
+		
+		q	glfwSwapBuffers(window);
 	}
 
 	cout << "Shutdown..." << endl;
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 	glfwTerminate();
 	return 0;
 }
